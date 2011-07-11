9d5858b417d744b489c8324267cd0d1895123d39
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-11 11:12:15 +0200

    Update settings.py to point to django-jquery-file-upload.urls
    Earlier I had it checked out as "upload" not the full name I gave it on
    github later.

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'upload.urls'
+ROOT_URLCONF = 'django-jquery-file-upload.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
