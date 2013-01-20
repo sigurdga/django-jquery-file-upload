e76eff56f8de5921c6554a4447867a1820fa476a
ET-CS <et.programming@gmail.com>
2013-01-21 01:05:33 +0200

    removed updated .js from original project. default / routing added.

--- settings.py
+++ settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'fileupload',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
