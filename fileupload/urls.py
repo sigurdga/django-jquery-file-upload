c3de83c79131a18bab5c8f36ee6afb0af1a6a0ce
ET-CS <et.programming@gmail.com>
2013-08-10 07:33:42 +0300

    Update urls.py to import patterns, url (not *)

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 from fileupload.views import PictureCreateView, PictureDeleteView
 
 urlpatterns = patterns('',
