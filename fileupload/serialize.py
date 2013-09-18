8fb62852e0289d826f750a72c32e909844a14d8d
João Paulo Dubas <joao.dubas@dev.magazineluiza.com.br>
2013-09-18 15:09:26 -0300

    Separate serialiazation and json response from view processing.

---
+++ fileupload/serialize.py
@@ -0,0 +1,36 @@
+# encoding: utf-8
+import mimetypes
+import re
+from django.core.urlresolvers import reverse
+
+
+def order_name(name):
+    """order_name -- Limit the name to 20 chars length, and convert to a
+    ellipsed string.
+
+    name -- text to be limited.
+
+    """
+    name = re.sub (r'^.*/', '', name)
+    if len(name)>20:
+        return name[:10] + "..." + name[-7:]
+    else:
+        return name
+
+
+def serialize(instance):
+    """serialize -- Serialize a Picture instance into a `json` object.
+
+    instance -- Picture instance
+    """
+    return {
+        'url': instance.file.url,
+        'name': order_name(instance.file.name),
+        'type': mimetypes.guess_type(instance.file.path)[0] or 'image/png',
+        'thumbnailUrl': instance.file.url,
+        'size': instance.file.size,
+        'deleteUrl': reverse('upload-delete', args=[instance.pk]),
+        'deleteType': 'DELETE',
+    }
+
+
--- fileupload/views.py
+++ fileupload/views.py
@@ -1,57 +1,37 @@
-from fileupload.models import Picture
-from django.views.generic import CreateView, DeleteView
+# encoding: utf-8
+from django.views.generic import CreateView, DeleteView, View
+from .models import Picture
+from .response import JSONResponse, response_mimetype
+from .serialize import serialize
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.utils import simplejson
-from django.core.urlresolvers import reverse
-
-from django.conf import settings
-import re
-
-def response_mimetype(request):
-    if "application/json" in request.META['HTTP_ACCEPT']:
-        return "application/json"
-    else:
-        return "text/plain"
-
-def orderName(name):
-    name = re.sub (r'^.*/', '', name)
-    if len(name)>20:
-        return name[:10] + "..." + name[-7:]
-    else:
-        return name
 
 class PictureCreateView(CreateView):
     model = Picture
 
     def form_valid(self, form):
         self.object = form.save()
-        f = self.request.FILES.get('file')
-        files = [{
-            'url': self.object.file.url,
-            'name': orderName(f.name),
-            "type": "image/png",
-            'thumbnailUrl': self.object.file.url,
-            'size': f.size,
-            'deleteUrl': reverse('upload-delete', args=[self.object.id]),
-            'deleteType': "DELETE",
-            }]
+        files = [serialize(self.object)]
         data = {"files": files}
-        response = JSONResponse(data, {}, response_mimetype(self.request))
+        response = JSONResponse(data, mimetype=response_mimetype(self.request))
         response['Content-Disposition'] = 'inline; filename=files.json'
         return response
+
 
 class BasicVersionCreateView(PictureCreateView):
     template_name_suffix = '_basic_form'
 
+
 class BasicPlusVersionCreateView(PictureCreateView):
     template_name_suffix = '_basicplus_form'
+
 
 class AngularVersionCreateView(PictureCreateView):
     template_name_suffix = '_angular_form'
 
+
 class jQueryVersionCreateView(PictureCreateView):
     template_name_suffix = '_jquery_form'
+
 
 class PictureDeleteView(DeleteView):
     model = Picture
@@ -59,28 +39,17 @@ class PictureDeleteView(DeleteView):
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         self.object.delete()
-        response = JSONResponse(True, {}, response_mimetype(self.request))
+        response = JSONResponse(True, mimetype=response_mimetype(request))
         response['Content-Disposition'] = 'inline; filename=files.json'
         return response
 
-def PictureListView(request):
-    files = []
-    for obj in Picture.objects.all():
-        files += [{
-        'name': orderName(obj.file.name),
-        'size': obj.file.size,
-        'url': obj.file.url,
-        'thumbnailUrl': obj.file.url,
-        'deleteUrl': reverse('upload-delete', args=[obj.id]),
-        'deleteType': "DELETE"
-        }]
-    data = {"files": files}
-    response = JSONResponse(data, {}, response_mimetype(request))
-    response['Content-Disposition'] = 'inline; filename=files.json'
-    return response
 
-class JSONResponse(HttpResponse):
-    """JSON response class."""
-    def __init__(self,obj='',json_opts={},mimetype="application/json",*args,**kwargs):
-        content = simplejson.dumps(obj,**json_opts)
-        super(JSONResponse,self).__init__(content,mimetype,*args,**kwargs)
+class PictureListView(View):
+    def get(self, request, *args, **kwargs):
+        files = []
+        for obj in Picture.objects.all():
+            files.append(serialize(obj))
+        data = {'files': files}
+        response = JSONResponse(data, mimetype=response_mimetype(request))
+        response['Content-Disposition'] = 'inline; filename=files.json'
+        return response
