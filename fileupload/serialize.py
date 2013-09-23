eaeaa6fb9bc328c9ee1012924ff0ac59d7590564
João Paulo Dubas <joao.dubas@dev.magazineluiza.com.br>
2013-09-23 17:17:22 -0300

    Minor improvements in order_name and serialize methods.

--- fileupload/serialize.py
+++ fileupload/serialize.py
@@ -5,30 +5,32 @@ from django.core.urlresolvers import reverse
 
 
 def order_name(name):
-    """order_name -- Limit the name to 20 chars length, and convert to a
-    ellipsed string.
+    """order_name -- Limit a text to 20 chars length, if necessary strips the
+    middle of the text and substitute it for an ellipsis.
 
     name -- text to be limited.
 
     """
     name = re.sub(r'^.*/', '', name)
-    if len(name)>20:
-        return name[:10] + "..." + name[-7:]
-    else:
+    if len(name) <= 20:
         return name
+    return name[:10] + "..." + name[-7:]
 
 
-def serialize(instance):
-    """serialize -- Serialize a Picture instance into a `json` object.
+def serialize(instance, file_attr='file'):
+    """serialize -- Serialize a Picture instance into a dict.
 
     instance -- Picture instance
+    file_attr -- attribute name that contains the FileField or ImageField
+
     """
+    obj = getattr(instance, file_attr)
     return {
-        'url': instance.file.url,
-        'name': order_name(instance.file.name),
-        'type': mimetypes.guess_type(instance.file.path)[0] or 'image/png',
-        'thumbnailUrl': instance.file.url,
-        'size': instance.file.size,
+        'url': obj.url,
+        'name': order_name(obj.name),
+        'type': mimetypes.guess_type(obj.path)[0] or 'image/png',
+        'thumbnailUrl': obj.url,
+        'size': obj.size,
         'deleteUrl': reverse('upload-delete', args=[instance.pk]),
         'deleteType': 'DELETE',
     }
