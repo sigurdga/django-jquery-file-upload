302e2f102b4fc003b4e278faa4f10393ca2aa7d1
Neil Newman <nnewman2@albany.edu>
2015-01-15 20:57:09 -0500

    Updated to support Django 1.7

--- settings.py
+++ settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django-jquery-file-upload.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
