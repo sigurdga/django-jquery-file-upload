da99c92b0f3ecb19d4b1b44ce99e96d33060ccfc
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-06-08 00:22:51 +0200

    Escaping rules has changed in Django 1.9.
    Using mark_safe for the template.
    
    Fixes #61

--- fileupload/templatetags/upload_tags.py
+++ fileupload/templatetags/upload_tags.py
@@ -1,10 +1,11 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.simple_tag
 def upload_js():
-    return """
+    return mark_safe("""
 <!-- The template to display files available for upload -->
 <script id="template-upload" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
@@ -73,7 +74,7 @@ def upload_js():
     </tr>
 {% } %}
 </script>
-"""
+""")
 
 
 
